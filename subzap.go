d649463611f48b0931181e2e3a9d0127c11f4e5b
yangyile <gogit@github.com/go-xlan/gogit>
2025-06-05 17:05:39 +0700

    增加skip功能

--- subzap.go
+++ subzap.go
@@ -17,3 +17,11 @@ func (T *Zap) SubZap(field zap.Field, fields ...zap.Field) *Zap {
 func (T *Zap) NewZap(k string, v string, fields ...zap.Field) *Zap {
 	return NewZap(T.NewLog(k, v, fields...))
 }
+
+func (T *Zap) SkipLog(skipDepth int, fields ...zap.Field) *zap.Logger {
+	return T.LOG.WithOptions(zap.AddCallerSkip(skipDepth)).With(fields...)
+}
+
+func (T *Zap) SkipZap(skipDepth int, fields ...zap.Field) *Zap {
+	return NewZap(T.SkipLog(skipDepth).With(fields...))
+}
--- subzap_test.go
+++ subzap_test.go
@@ -22,3 +22,25 @@ func TestZap_NewZap(t *testing.T) {
 	zp.LOG.Error("msg", zap.Int("a", 1), zap.Int("b", 2))
 	zp.SUG.Error(1, 2, 3)
 }
+
+func TestZap_SkipLog(t *testing.T) {
+	zapLog := zaplog.LOGGER.SubZap(zap.String("module", "skip-log-test-case"))
+	zapLog.LOG.Debug("msg", zap.Int("a", 1), zap.Int("b", 2))
+
+	run := func(t *testing.T) {
+		subLog := zapLog.SkipLog(1, zap.Int("c", 3))
+		subLog.Debug("abc", zap.Int("a", 1), zap.Int("b", 2))
+	}
+	run(t)
+}
+
+func TestZap_SkipZap(t *testing.T) {
+	zapLog := zaplog.LOGGER.SubZap(zap.String("module", "skip-zap-test-case"))
+	zapLog.LOG.Debug("msg", zap.Int("a", 1), zap.Int("b", 2))
+
+	run := func(t *testing.T) {
+		subZap := zapLog.SkipZap(1, zap.Int("c", 3))
+		subZap.LOG.Debug("abc", zap.Int("a", 1), zap.Int("b", 2))
+	}
+	run(t)
+}
